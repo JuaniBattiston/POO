98e402879de78680cc1b092e87781473c670cc12
Juan Ignacio Battiston <68969090+Batucho@users.noreply.github.com>
2024-03-18 19:53:10 -0300

    Clase 3 - Ej3

---
+++ Clase 3/Ej3/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+namespace POO {
+
+void getVersion(){
+    cout << "Lib: V0.0.1 - 20240318" << endl;
+}
+
+}
+
+int main()
+{
+    POO::getVersion();
+    return 0;
+}
