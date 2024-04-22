335b6f8abfaf1fb964caae4cd09f533f45a3a397
Juan Ignacio Battiston <68969090+Batucho@users.noreply.github.com>
2024-04-22 19:56:40 -0300

    Ej D

--- Clase 9/Base/login.cpp
+++ Clase 9/Base/login.cpp
@@ -1,15 +1,89 @@
 #include "login.h"
 #include "ui_login.h"
+#include <QFile>
+#include <QDebug>
+#include <QMessageBox>
+#include <QPainter>
+#include <QGridLayout>
+#include <QLabel>
 
 login::login(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::login)
+    , manager(new QNetworkAccessManager(this))
+    , index(0)
 {
     ui->setupUi(this);
+    get_all_ulrs_from_file();
+
+    QVBoxLayout *layout = new QVBoxLayout(this);
+
+    button_prev = new QPushButton("Anterior", this);
+    button_next = new QPushButton("Siguiente", this);
+    QHBoxLayout *buttonLayout = new QHBoxLayout;
+
+    buttonLayout->addWidget(button_prev);
+    buttonLayout->addWidget(button_next);
+    layout->addStretch(1);
+    layout->addLayout(buttonLayout);
+
+    setLayout(layout);
+
+    connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(downloadFinished(QNetworkReply*)));
+    connect(button_next, SIGNAL(clicked()), this, SLOT(slot_next()));
+    connect(button_prev, SIGNAL(clicked()), this, SLOT(slot_prev()));
 }
 
 login::~login()
 {
     delete ui;
+}
+
+void login::get_all_ulrs_from_file()
+{
+    QFile file("text.txt");
+    if(!file.open(QIODevice::ReadOnly)) {
+        QMessageBox::information(0, "error", file.errorString());
+    }
+
+    QTextStream in(&file);
+
+    while(!in.atEnd()) {
+        QString line = in.readLine();
+        manager->get(QNetworkRequest(QUrl(line)));
+    }
+
+    file.close();
+}
+
+void login::paintEvent(QPaintEvent *)
+{
+    QPainter p(this);
+
+    if (!vec.empty()) {
+        index = qBound(0, index, vec.size() - 1);
+        p.drawImage(0, 0, vec.at(index).scaled(this->size()), Qt::KeepAspectRatio);
+    }
+}
+
+void login::downloadFinished(QNetworkReply *reply)
+{
+    QImage downloaded_image = QImage::fromData(reply->readAll());
+    vec.push_back(downloaded_image);
+    if (vec.size() == 1) {
+        repaint();
+    }
+}
+
+void login::slot_next()
+{
+    index++;
+    repaint();
+}
+
+void login::slot_prev()
+{
+    index--;
+    repaint();
 }
 
--- Clase 9/Base/login.h
+++ Clase 9/Base/login.h
@@ -2,6 +2,14 @@
 #define LOGIN_H
 
 #include <QWidget>
+#include <QMainWindow>
+#include <QImage>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QUrl>
+#include <QPushButton>
+#include <QLabel>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class login; }
@@ -15,7 +23,26 @@ public:
     login(QWidget *parent = nullptr);
     ~login();
 
+    void get_all_ulrs_from_file();
+
+protected:
+    void paintEvent(QPaintEvent *);
+
+private slots:
+    void downloadFinished(QNetworkReply* reply);
+    void slot_next();
+    void slot_prev();
+
 private:
     Ui::login *ui;
+    QImage image;
+    QNetworkAccessManager *manager;
+    QPushButton *button_prev;
+    QPushButton *button_next;
+    QLabel *img;
+    int index;
+    QVector<QImage> vec;
+
 };
+
 #endif // LOGIN_H
